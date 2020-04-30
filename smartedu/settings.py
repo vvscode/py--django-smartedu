bd65a4ab97f0d746263768b59541c6fb99e973c1
Vasil Vanchuk <v.vanchuk@tut.by>
2020-04-30 11:51:45 +0300

    Add SECRET_KEY to env setup

--- smartedu/settings.py
+++ smartedu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm-@wbvt&(lzz-^267$t2zhs2xl8t)lrj#-8vdc&e3k&*7$6)i$'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'm-@wbvt&(lzz-^267$t2zhs2xl8t)lrj#-8vdc&e3k&*7$6)i$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
