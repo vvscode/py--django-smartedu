b3fe0c71837029700e6d0297e2940220dff0cc61
Vasil Vanchuk <v.vanchuk@tut.by>
2020-05-01 22:20:24 +0300

    Fix superuser creation

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -3,7 +3,7 @@
 python manage.py migrate
 
 # https://docs.djangoproject.com/en/3.0/ref/django-admin/#django-admin-createsuperuser
-# DJANGO_SUPERUSER_USERNAME and  DJANGO_SUPERUSER_PASSWORD are used
-python manage.py createsuperuser --noinput
+# DJANGO_SUPERUSER_USERNAME and  DJANGO_SUPERUSER_PASSWORD, DJANGO_SUPERUSER_EMAIL are used
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 
 python manage.py training_sheet_seed
