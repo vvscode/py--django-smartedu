4ab6792d7c5e9d20836eeb5defd3408a43f1053b
Vasil Vanchuk <v.vanchuk@tut.by>
2020-05-02 00:51:23 +0300

    Migrate code to CBV

--- training_sheet/urls.py
+++ training_sheet/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="training_sheet.index"),
-    path("course/<int:course_id>/", views.course, name="training_sheet.course"),
+    path("", views.CoursesListClassView.as_view(), name="training_sheet.index"),
+    path(
+        "course/<int:course_id>/",
+        views.CourseClassView.as_view(),
+        name="training_sheet.course",
+    ),
 ]
--- training_sheet/views.py
+++ training_sheet/views.py
@@ -1,19 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
+from django.views import View
+
 from training_sheet.models import Course
 
 
-def index(request):
-    return render(
-        request, "training_sheet/index.html", context={"courses": Course.objects.all()}
-    )
+class CoursesListClassView(View):
+    def get(self, request):
+        return render(
+            request,
+            "training_sheet/index.html",
+            context={"courses": Course.objects.all()},
+        )
 
 
-def course(request, course_id):
-    return render(
-        request,
-        "training_sheet/course.html",
-        context={"course": Course.objects.get(pk=course_id)},
-    )
+class CourseClassView(View):
+    def get(self, request, course_id):
+        return render(
+            request,
+            "training_sheet/course.html",
+            context={"course": get_object_or_404(Course, pk=course_id)},
+        )
