121f8523797adc465ea5ee4e6cc3a095b0bd3512
Vasil Vanchuk <v.vanchuk@tut.by>
2020-05-01 19:56:32 +0300

    Format code. Change fields size

--- smartedu/urls.py
+++ smartedu/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('training_sheet/', include('training_sheet.urls')),
-    path('admin/', admin.site.urls),
+    path("training_sheet/", include("training_sheet.urls")),
+    path("admin/", admin.site.urls),
 ]
--- training_sheet/models.py
+++ training_sheet/models.py
@@ -1,38 +1,60 @@
 from django.db import models
+
 # https://habr.com/ru/post/313764/#OneToOneField
 from django.contrib.auth.models import User
 
+
 class Teacher(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  job_title = models.CharField(max_length=300)
-  about_description = models.TextField()
-  
-  def __str__(self):
-    return f'Teacher #{self.job_title}'
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    job_title = models.CharField(max_length=250)
+    about_description = models.TextField()
+
+    def __str__(self):
+        return f"Teacher #{self.job_title}"
+
+    class Meta:
+        pass
+
 
 class Student(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  def __str__(self):
-    return f'Student'
-  
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Student"
+
+
 class Tag(models.Model):
-  title = models.CharField(max_length=200)
-  slug = models.CharField(max_length=200)
-  def __str__(self):
-    return f'Tag #{self.title}'
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return f"Tag #{self.title}"
+
+    class Meta:
+        pass
+
 
 class Course(models.Model):
-  title = models.CharField(max_length=300)
-  description = models.TextField()
-  tags = models.ManyToManyField(Tag)
-  course_teachers = models.ManyToManyField(Teacher)
-  def __str__(self):
-    return f'Tag #{self.title}'
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    course_teachers = models.ManyToManyField(Teacher)
+
+    def __str__(self):
+        return f"Course #{self.title}"
+
+    class Meta:
+        pass
+
 
 class Group(models.Model):
-  group_name = models.CharField(max_length=300)
-  course = models.ForeignKey(Course, on_delete=models.PROTECT)
-  students = models.ManyToManyField(Student)
-  group_teachers = models.ManyToManyField(Teacher)
-  def __str__(self):
-    return f'Group: {self.group_name}'
+    group_name = models.CharField(max_length=250)
+    course = models.ForeignKey(Course, on_delete=models.PROTECT)
+    students = models.ManyToManyField(Student)
+    group_teachers = models.ManyToManyField(Teacher)
+
+    def __str__(self):
+        return f"Group: {self.group_name}"
+
+    class Meta:
+        pass
