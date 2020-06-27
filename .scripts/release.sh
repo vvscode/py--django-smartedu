66fab83941b98997e4e6cceb3bcf7a3d6c9201bc
Vasil Vanchuk <v.vanchuk@tut.by>
2020-06-27 12:04:12 +0300

    Remove username field from CustomUser

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -6,6 +6,6 @@ python manage.py migrate
 
 # https://docs.djangoproject.com/en/3.0/ref/django-admin/#django-admin-createsuperuser
 # DJANGO_SUPERUSER_USERNAME and  DJANGO_SUPERUSER_PASSWORD, DJANGO_SUPERUSER_EMAIL are used
-python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL
 
 python manage.py training_sheet_seed
