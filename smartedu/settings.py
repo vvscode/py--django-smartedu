1ca4e75fbb69e465a01a741e6005c0ebc4e7f6ca
Vasil Vanchuk <v.vanchuk@tut.by>
2020-07-12 19:43:02 +0300

    Add corsheaders middleware

--- smartedu/settings.py
+++ smartedu/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
